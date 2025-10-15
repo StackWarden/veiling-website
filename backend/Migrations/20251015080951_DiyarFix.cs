8c76086764488a6f57bfb3d9660ca2c1dd6405e3
joeribrinks <joeri1314@gmail.com>
2025-10-15 10:10:15 +0200

    add migration cause diyar is lazy

---
+++ backend/Migrations/20251015080951_DiyarFix.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class DiyarFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AuctionItems",
+                table: "AuctionItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AuctionItems_AuctionId",
+                table: "AuctionItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AuctionItems",
+                table: "AuctionItems",
+                columns: new[] { "AuctionId", "ProductId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AuctionItems",
+                table: "AuctionItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AuctionItems",
+                table: "AuctionItems",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuctionItems_AuctionId",
+                table: "AuctionItems",
+                column: "AuctionId");
+        }
+    }
+}
