b9b29fbdfac81f4e54672ef5976421b6093fba8a
Sitjn64 <Stijnlammers2005@gmail.com>
2025-10-15 09:07:16 +0200

    Update AppDbContext.cs

--- backend/Db/AppDbContext.cs
+++ backend/Db/AppDbContext.cs
@@ -15,4 +15,14 @@ public class AppDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     public DbSet<Product> Products { get; set; }
     public DbSet<Auction> Auctions { get; set; }
     public DbSet<AuctionItem> AuctionItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AuctionItem>()
+            .HasOne(e => e.Product)
+            .WithMany(e => e.AuctionItems)
+            .HasForeignKey(e => e.ProductId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.SetNull);
+    }
 }
