f763c151a230dbd17f2fde5082e769cae0bb88f5
joeribrinks <joeri1314@gmail.com>
2025-10-28 10:32:31 +0100

    connection

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import ExampleComponent from "@/components/exampleComponent";
+import AuctionList from "@/components/getAuctions";
 
 export default function Home() {
   return (
     <div>
       <main>
         <ExampleComponent />
+        <AuctionList />
       </main>
     </div>
   );
