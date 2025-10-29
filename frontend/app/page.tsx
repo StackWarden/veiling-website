87596e919a16cb65c3853b64ab09eac6983b5b5a
joeribrinks <joeri1314@gmail.com>
2025-10-29 11:11:19 +0100

    nukes example

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <div>
         <Container>
-          <ExampleComponent />
           <GetAuctions />
           <PostAuction />
         </Container>
