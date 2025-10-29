3d2580f726b7413dacb8dcc16826b97ad31d5aad
joeribrinks <joeri1314@gmail.com>
2025-10-29 11:13:09 +0100

    oops

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,6 @@
 import Container from "@/components/container";
 import GetAuctions from "@/components/api/auctions/getAuctions";
 import PostAuction from "@/components/api/auctions/postAuction";
-import ExampleComponent from "@/components/exampleComponent";
 
 export default function Home() {
   return (
