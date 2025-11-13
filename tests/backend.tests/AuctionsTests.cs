3dc9df65d2c93a276d002ed4394bee85f23cfe90
joeribrinks <joeri1314@gmail.com>
2025-11-13 11:39:39 +0100

    test get with specific id

--- tests/backend.tests/AuctionsTests.cs
+++ tests/backend.tests/AuctionsTests.cs
@@ -2,8 +2,9 @@
 
 using System.Net;
 using FluentAssertions;
-using Xunit;
 using System.Net.Http.Json;
+using System.Text.Json;
+using Xunit;
 
 public class AuctionTests : IClassFixture<TestWebAppFactory>
 {
@@ -14,14 +15,48 @@ public class AuctionTests : IClassFixture<TestWebAppFactory>
         _client = factory.CreateClient();
     }
 
+    // --------------------------------------------------GET REQUESTS--------------------------------------------------
     // GET /auctions test
     [Fact(DisplayName = "[GET /auctions returns 200 OK]")]
     public async Task GetAuctions()
     {
         var response = await _client.GetAsync("/auctions");
+
+        // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
 
+    [Fact(DisplayName = "[GET /auctions with specific ID & returns 200 OK]")]
+    public async Task GetAuctionsWithID()
+    {
+        // Create an auction else we can't get it
+        var body = new
+        {
+            auctionneerId = Guid.NewGuid(),
+            startTime = DateTime.Parse("2025-12-01T09:00:00Z"),
+            endTime = DateTime.Parse("2025-12-01T10:00:00Z"),
+            status = "Planned"
+        };
+
+        var createResponse = await _client.PostAsJsonAsync("/auctions", body);
+        createResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        // Read returned JSON from the POST to get the auction ID
+        var createdAuction = await createResponse.Content.ReadFromJsonAsync<JsonElement>();
+        var id = createdAuction.GetProperty("id").GetGuid();
+
+        // GET /auctions/{id}
+        var getResponse = await _client.GetAsync($"/auctions/{id}");
+        getResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        // Deserialize into C# object
+        var auction = await getResponse.Content.ReadFromJsonAsync<JsonElement>();
+
+        // Assert
+        auction.GetProperty("status").GetString().Should().Be("Planned");
+    }
+
+    // --------------------------------------------------POST REQUESTS--------------------------------------------------
     // POST /auctions test
     [Fact(DisplayName = "[POST /auctions creates an auction]")]
     public async Task CreateAuction()
