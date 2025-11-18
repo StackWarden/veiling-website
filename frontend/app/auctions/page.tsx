ff9feee56c611c34fcc51a036e639b0aae618125
joeribrinks <joeri1314@gmail.com>
2025-11-18 10:28:28 +0100

    lint

--- frontend/app/auctions/page.tsx
+++ frontend/app/auctions/page.tsx
@@ -1,13 +1,12 @@
-import Container from "@/components/container"
 import AuctionsDashboard from "@/components/AuctionsDashboard";
 import Header from "@/components/header"
 
 export default function AuctionsPage() {
   
   return (
-        <>
+    <>
       <Header />
-       <AuctionsDashboard></AuctionsDashboard>
+       <AuctionsDashboard />
     </>
            
   );
