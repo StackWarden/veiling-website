36d29a53bf3f4d48f0bc455f4a98be4d52be0928
joeribrinks <joeri1314@gmail.com>
2026-01-15 14:35:21 +0100

    show won auctions

---
+++ frontend/app/won-auctions/page.tsx
@@ -0,0 +1,13 @@
+import WonAuctionsDashboard from "@/components/auctions/wonAuctionsList";
+import Header from "@/components/header"
+
+export default function WonAuctionsPage() {
+  
+  return (
+    <>
+      <Header />
+      <WonAuctionsDashboard />
+    </>
+           
+  );
+}
